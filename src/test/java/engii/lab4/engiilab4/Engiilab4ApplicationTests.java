c474060610c846d74c3e590b542648733ba4b90f
Kerlyson <kerlyson_sk@yahoo.com.br>
2020-01-31 13:49:55 -0300

    testes

--- src/test/java/engii/lab4/engiilab4/Engiilab4ApplicationTests.java
+++ src/test/java/engii/lab4/engiilab4/Engiilab4ApplicationTests.java
@@ -2,6 +2,7 @@ package engii.lab4.engiilab4;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class Engiilab4ApplicationTests {
